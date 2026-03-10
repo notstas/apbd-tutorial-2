977692e04ef5925bd0cc257bdd3c6fd7e80d9815
notstas <noemail@pjwstk.edu.pl>
2026-03-10 16:27:06 +0100

    feat(stats): created StatisticsHelper and basic method to calculate average

---
+++ StatisticsHelper.cs
@@ -0,0 +1,15 @@
+﻿namespace tutorial_2;
+
+public class StatisticsHelper
+{
+    public static int calculateAverage(int[] items)
+    {
+        var sum = 0;
+        foreach (var num in items)
+        {
+            sum += num;
+        }
+        
+        return sum / items.Length;
+    }
+}
